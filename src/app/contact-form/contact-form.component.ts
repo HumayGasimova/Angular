6d23504c1b7de73362b701f4a00a680d6d3556ee
Humay <crypto347@gmail.com>
2019-07-15 14:12:51 +0400

    fixed

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -3,7 +3,7 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+
 })
 export class ContactFormComponent{
 
