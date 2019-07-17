b6f1e3cb0e3043948f2c5b695447f4fbedf3801e
Humay <crypto347@gmail.com>
2019-07-17 11:07:07 +0400

    Http Post

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,3 +1,4 @@
+import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
@@ -7,13 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent {
-   posts: any;
-  constructor(http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
-      .subscribe((response) => {
-        this.posts = response;
-      })
+  posts: any;
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) {
+  http.get(this.url)
+    .subscribe((response) => {
+      this.posts = response;
+    })
   }
 
-
+  createPost(input: HTMLInputElement){
+    let post = { title: input.value};
+    input.value = '';
+    this.http.post(this.url, post)
+      .subscribe((response)=>{
+        post['id'] = response;
+        this.posts.splice(0,0,post)
+        console.log(response)
+      })
+      // .toPromise().then(res => {
+      //   console.log(res)
+      // }
+      // )
+      // .pipe(map((res:any) => {
+      //   post['id'] = res.json().id;
+      //   this.posts.splice(0,0,post)
+      //   console.log(res)
+      // }
+      
+      //  ))
+  }
 }
