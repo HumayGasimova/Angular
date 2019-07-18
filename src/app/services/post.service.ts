ecc56f17c443ded708de286468037079dea953f0
Humay <crypto347@gmail.com>
2019-07-18 14:30:44 +0400

    Extracting a reusable error handing method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -16,31 +16,32 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url)
+    .catch(this.handleError);
+
   }
 
   createPosts(post){
     return this.http.post(this.url, post)
-    .catch((error: Response) => {
-      if(error.status === 400){
-        return Observable.throw(new BadInput(JSON.parse(JSON.stringify(error))))
-      }else{
-        return Observable.throw(JSON.parse(JSON.stringify(error)))
-      }
-    })
+    .catch(this.handleError);
   }
 
   updatePost(post){
     return this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true})) 
+    .catch(this.handleError);
   }
 
   deletePost(id){
     return this.http.delete(this.url + '/' + id)
-      .catch((error: Response) => {
-        if(error.status === 404){
-          return Observable.throwError(new NotFoundError());
-        }
-        return Observable.throwError(new AppError(error));
-      });
+      .catch(this.handleError);
+  }
+  private handleError(error: Response){
+    if(error.status === 400)
+      return Observable.throw(new BadInput(JSON.parse(JSON.stringify(error))))
+      
+    if(error.status === 404)
+      return Observable.throwError(new NotFoundError());
+      
+      return Observable.throwError(new AppError(error));
   }
 }
