e687a0c9884465ef16058b6abeac337c9ceca766
Humay <crypto347@gmail.com>
2019-07-23 13:28:06 +0400

    Show or Hide element base on the UsersRole(decoded token)

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -40,5 +40,13 @@ export class AuthService {
 
     // return !isExpired;
   //}
-}
+ }
+
+ get currentUser() {
+   let token = localStorage.getItem('token');
+   if(!token){
+     return null;
+   }
+   return new JwtHelper().decodeToken(token);
+ }
 }
