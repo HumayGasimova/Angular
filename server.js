5f0d324979c880dae96e85dd911c4883c5357c69
Crypto347 <Crypto347>
2019-07-25 16:08:42 +0400

    Prepare for heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app =express();
+
+app.use(express.static(__dirname + '/dist'));
+
+//....Api endpoins
+
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
