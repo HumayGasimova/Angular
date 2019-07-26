805795c71c1888b44136bf33c5731abaac3ba542
Humay <crypto347@gmail.com>
2019-07-26 17:30:31 +0400

    Firebase + Observables unsubscribe()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase, AngularFireList  } from 'angularfire2/database';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
  
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   courses: any[];
+  subscription: Subscription;
 
   constructor( private db: AngularFireDatabase) {
-    db.list('courses')
+    this.subscription = db.list('/courses')
     .valueChanges()
     .subscribe(courses => {
       this.courses = courses;
       console.log(courses)
     })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
